48bc1eb9dd80a5fa7e6089383a53797658d425fb
nv8red <nv8red@gmail.com>
2018-03-13 11:21:40 -0700

    Update funcs.h

--- python_interface/funcs.h
+++ python_interface/funcs.h
@@ -1,5 +1,6 @@
 
 
 void writeDataBRAM();
-/* 
-*/
+//Writes the weights and the data into the BRAM
+//Waits for the uart to send the start signal first
+//Writes the number of layers, then the weights, then the data size, then the data
